5fc8a723352b484b526b7c8a75ad34582b1a56b7
Alexander <ale097@csiro.au>
2019-10-10 09:06:46 +1100

    Filtering out sum counts <100

--- scripts/tidy_count.R
+++ scripts/tidy_count.R
@@ -13,3 +13,14 @@ Redlands_counts
 
 redlands_horse_metadata
 
+counts_morethan100 <- Redlands_counts %>% 
+  mutate(sum_counts = s1 +s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9 + s10 + s11 + s12 + s13 + s14 + s15) %>% 
+  filter(sum_counts >= 100)
+
+
+
+plot_all <- ggplot(data = counts_morethan100,
+                        mapping = aes(x = , y = Temp_min, group = )) + geom_point(alpha = 0.2)
+
+
+?rowSums
