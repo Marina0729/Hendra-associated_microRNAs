51afa9c6ce4671d8e953760f87fb429d4d71f946
Alexander <ale097@csiro.au>
2019-10-10 08:30:32 +1100

    Initial commit

---
+++ scripts/tidy_count.R
@@ -0,0 +1,15 @@
+.libPaths(c("C:/Users/ale097/Data School/Packages"))
+library(tidyverse)
+library(cowplot)
+
+read_csv("data/Redlands_counts.csv")
+Redlands_counts <- read_csv("Data/Redlands_counts.csv")
+
+read_csv("data/redlands_horse_metadata.csv")
+redlands_horse_metadata <- read_csv("Data/redlands_horse_metadata.csv")
+
+
+Redlands_counts
+
+redlands_horse_metadata
+
