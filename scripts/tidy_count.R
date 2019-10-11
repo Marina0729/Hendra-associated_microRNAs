eb60f9cb316b6616b64730f2154d30894c79e346
Alexander <ale097@csiro.au>
2019-10-11 13:03:48 +1100

    Filtering to remove lowly expressed genes

--- scripts/tidy_count.R
+++ scripts/tidy_count.R
@@ -2,24 +2,48 @@
 library(tidyverse)
 library(cowplot)
 
-read_csv("data/Redlands_counts.csv")
-Redlands_counts <- read_csv("Data/Redlands_counts.csv")
+read_csv("data/Redlands_counts.csv")                                    #Read in the data 
+microRNA_counts <- read_csv("Data/Redlands_counts.csv")                 #name it microRNA_counts
 
-read_csv("data/redlands_horse_metadata.csv")
-redlands_horse_metadata <- read_csv("Data/redlands_horse_metadata.csv")
+read_csv("data/redlands_horse_metadata.csv")                            #Read in the metadata
+redlands_horse_metadata <- read_csv("Data/redlands_horse_metadata.csv") #Name it redlands_horse_metadata
 
 
-Redlands_counts
+microRNA_counts
 
 redlands_horse_metadata
 
-#tidy the count data 
-redlands_counts_tidy <- Redlands_counts %>%              #assign to new variable
-  gather(sample, counts, -gene) %>%                      #gather to get sample names into a column
-  left_join(redlands_horse_metadata, by = "sample") %>%  #join by sample names
-  rename(day = condition) %>%                            #rename column 
-  select(-sample) %>%                                    #remove sample column
-  mutate()
+#Filtering to remove lowly expressed genes
+#Genes with very low counts across all libraries provide little evidence for differential expression and they 
+#interfere with some of the statistical approximations that are used later in the pipeline. They also add to 
+#the multiple testing burden when estimating false discovery rates, reducing power to detect differentially 
+#expressed genes. These genes should be filtered out prior to further analysis.
+
+#Install edgeR package
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("edgeR")
+
+
+
+Day0counts <- microRNA_counts["s1", "s6", "s11"]
+
+microRNA_counts %>% 
+  select()
+
+
+
+
+
+#tidy the microRNA_count data and join to metadata 
+microRNA_counts_tidy <- microRNA_counts %>%                             #assign to new variable
+  gather(sample, counts, -gene) %>%                                     #gather to get sample names into a column
+  left_join(redlands_horse_metadata, by = "sample") %>%                 #join by sample names
+  rename(day = condition) %>%                                           #rename column 
+  select(-sample)                                                       #remove sample column
+  
 
 
 #making some plots
